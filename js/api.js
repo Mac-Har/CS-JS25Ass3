a675c339aac92aa29c5e8fa031158f4ad88aa88c
Mac-Har <200615255@student.georgianc.on.ca>
2025-08-05 11:17:25 -0400

    IMG API testing

--- js/api.js
+++ js/api.js
@@ -1,9 +1,19 @@
 const baseFactURL = "https://meowfacts.herokuapp.com/";
 const baseImgURL = "https://http.cat/";
 
+var statusCode =404;
+//Fact API
 fetch(baseFactURL, {
 }).then(res => {
+    console.log(res.status);
+    statusCode = res.status;
     return res.json();
 }).then(json =>{
     console.log(json)
+})
+
+//IMG API
+fetch (baseImgURL+statusCode, {
+}).then(res=>{
+    console.log(res);
 })
