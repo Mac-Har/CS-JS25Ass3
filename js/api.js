1c2631890b9c0a2e4b44a97d31e5def7e43e5460
Mac-Har <200615255@student.georgianc.on.ca>
2025-08-05 12:35:05 -0400

    Finishing up

--- js/api.js
+++ js/api.js
@@ -1,13 +1,16 @@
-const baseFactURL = "s";
+//API urls
+const baseFactURL = "https://meowfacts.herokuapp.com/";
 const baseImgURL = "https://http.cat/";
-
+//query selectors
 const code = document.querySelector("#code");
 const statusimg = document.querySelector("#statusimg");
 const output = document.querySelector("#output");
 const overridebtn = document.querySelector("#overridebtn");
+const newfactbtn = document.querySelector("#newfactbtn");
 const overtxt = document.querySelector("#overtxt")
-
+//Buttons
 overridebtn.addEventListener("click", overrideStatusImage)
+newfactbtn.addEventListener("click", getCatFact)
 
 var statusCode = 404;
 //Fact API
@@ -30,12 +33,12 @@ function setCatFact(json) {
 function setStatusImage() {//Normal API request doesn't work, so I directly set the image's src
     statusimg.src = baseImgURL+statusCode;
 }
-
+//If the user wants to see other status codes
 function overrideStatusImage() {
     statusCode = overtxt.value;
-    setStatusImage();
+    setStatusImage(); // https://http.cat/ to see all the images
 }
-
+//Initialize the site
 getCatFact();
 
 //IMG API - CORS error when fetching directly
