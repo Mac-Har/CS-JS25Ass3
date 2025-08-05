b3186e5d25ae12e42b5bd7f077c627b0119ac43e
Mac-Har <200615255@student.georgianc.on.ca>
2025-08-04 21:04:20 -0400

    Basic json testing

--- js/api.js
+++ js/api.js
@@ -1,3 +1,9 @@
 const baseFactURL = "https://meowfacts.herokuapp.com/";
 const baseImgURL = "https://http.cat/";
 
+fetch(baseFactURL, {
+}).then(res => {
+    return res.json();
+}).then(json =>{
+    console.log(json)
+})
