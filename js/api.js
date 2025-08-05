b1ceb4ebe4621ffc49926f9a18669559e64f437a
Mac-Har <200615255@student.georgianc.on.ca>
2025-08-05 12:28:40 -0400

    Connected the fact request api status to the image and also allow user to override to see other images

--- js/api.js
+++ js/api.js
@@ -1,27 +1,42 @@
-const baseFactURL = "https://meowfacts.herokuapp.com/";
+const baseFactURL = "s";
 const baseImgURL = "https://http.cat/";
 
 const code = document.querySelector("#code");
 const statusimg = document.querySelector("#statusimg");
 const output = document.querySelector("#output");
 const overridebtn = document.querySelector("#overridebtn");
+const overtxt = document.querySelector("#overtxt")
 
-overridebtn.addEventListener("click",setStatusImage)
+overridebtn.addEventListener("click", overrideStatusImage)
 
 var statusCode = 404;
 //Fact API
-fetch(baseFactURL, {
-}).then(res => {
-    console.log(res.status);
-    statusCode = res.status;
-    return res.json();
-}).then(json =>{
-    console.log(json);
-})
-
+function getCatFact(){ //Credit for documentation: https://alexwohlbruck.github.io/cat-facts/
+    fetch(baseFactURL, {
+    }).then(res => {
+        console.log(res.status);
+        statusCode = res.status;
+        setStatusImage();
+        return res.json();
+    }).then(json =>{
+        console.log(json);
+        setCatFact(json);
+    })
+    
+}
+function setCatFact(json) {
+    output.textContent = json.data;
+}
 function setStatusImage() {//Normal API request doesn't work, so I directly set the image's src
     statusimg.src = baseImgURL+statusCode;
 }
+
+function overrideStatusImage() {
+    statusCode = overtxt.value;
+    setStatusImage();
+}
+
+getCatFact();
 
 //IMG API - CORS error when fetching directly
 /*
